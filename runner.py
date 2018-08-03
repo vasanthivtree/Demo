030c0ffb6c4171dac43689e88835f42dd1714da6
vasanthivtree <42056136+vasanthivtree@users.noreply.github.com>
2018-08-03 11:16:11 +0530

    fifth commit

--- runner.py
+++ runner.py
@@ -1,1 +1,13 @@
 print "test runner"
+def prime(number):
+    for num in range(2,number):
+        status = True
+        for i in range(2,num):
+            if num % i == 0:
+                status = False
+        if status:
+            print(num)
+
+
+prime(100)
+
