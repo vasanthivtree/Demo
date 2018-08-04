aaa43be49ac14761520a8777bad7b86822d343b6
vasanthivtree <vasantha.ayyappan@ivtree.com>
2018-08-04 13:06:49 +0530

    sixth commit

---
+++ test.py
@@ -0,0 +1,12 @@
+def prime(number):
+    for num in range(2,number):
+        status = True
+        for i in range(2,num):
+            if num % i == 0:
+                status = False
+        if status:
+            print(num)
+
+
+prime(100)
+
